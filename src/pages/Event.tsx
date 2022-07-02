ca2c95fa48fc03d4e9def7ec8d3f14a5ffe3d12a
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-02 10:28:17 -0300

    projeto base criado

---
+++ src/pages/Event.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
+import { Video } from '../components/Video';
+
+export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+
+  const [sidebarOpened, setSidebarOpened] = useState(false);
+
+  function handleOpenSidebar() {
+    setSidebarOpened(!sidebarOpened);
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header
+        onSidebarOpened={handleOpenSidebar}
+        sidebarOpened={sidebarOpened}
+      />
+      <main className="flex flex-1">
+        {slug ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className="flex-1" />
+        )}
+        {(window.innerWidth >= 1024 || sidebarOpened) && (
+          <Sidebar />
+        )}
+      </main>
+    </div>
+  );
+}
